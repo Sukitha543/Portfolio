7e76f5d6406589febeeae502d345f0410e780e5e
Sukitha Chathuranga Hettimudalige <sukithachathuranga@gmail.com>
2025-06-22 22:02:43 +0530

    Add Footer and data.json

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const nav = document.querySelectorAll('header a');
+const navHeader = document.querySelectorAll('header a');
+const navFooter = document.querySelectorAll('footer a');
 
-nav.forEach(link => {
+navHeader.forEach(link => {
+    link.addEventListener("click",function(event){
+        event.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView();
+    });
+});
+
+navFooter.forEach(link => {
     link.addEventListener("click",function(event){
         event.preventDefault();
         const targetId = this.getAttribute("href");
