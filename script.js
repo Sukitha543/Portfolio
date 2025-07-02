9f5aaf90c0590cbfc815ee9ee32fac626a637997
Sukitha Chathuranga Hettimudalige <sukithachathuranga@gmail.com>
2025-07-02 20:14:29 +0530

    Add Hamburger Menu

--- script.js
+++ script.js
@@ -52,5 +52,9 @@ navHeader.forEach(link => {
     });
 });
 
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
- 
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
